f2ae62267bbc84369af62f2c9556d3fabc89b6a4
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2021-03-18 00:00:10 +0330

    work on api

--- app/Http/Controllers/Api/NumberQueryController.php
+++ app/Http/Controllers/Api/NumberQueryController.php
@@ -2,53 +2,75 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\SearchRequest;
+use Hsy\Ngn\Exceptions\NotDefinedPreNumber;
 use App\Http\Controllers\Controller;
 use Hsy\Ngn\Center;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class NumberQueryController extends Controller
 {
+
+    const RES_OK = 100;
+    const RES_NOT_FIND = 110;
+    const RES_BAD_ENTRY = 111;
+    const RES_NOT_DEFINED_PRE_NUMBERS = 112;
+    const RES_ERROR = 120;
+
     public function search(Request $request)
     {
+        $validator = \Validator::make($request->all(), $this->searchRequestValidationRules());
 
+        $preNumber = $request->pre_number;
+        $midNumber = $request->mid_number;
+        $number = $request->number;
 
-        $validator = \Validator::make($request->all(), [
-            "pre_number" => "required|digits:3",
-            "mid_number" => "required|digits:4",
-            "number" => "required|digits:4",
-        ]);
 
         if ($validator->fails())
-            return $this->error($validator->errors(), 422);
-
+            return $this->error($validator->errors(), self::RES_BAD_ENTRY);
 
         try {
-            $ngnCenter = new Center();
-            $result = $ngnCenter->search($request->pre_number, $request->mid_number, $request->number);
 
+            $ngnCenter = new Center();
+            $result = $ngnCenter->search($preNumber, $midNumber, $number);
+
+        } catch (NotDefinedPreNumber $e) {
+            $message = "Not defined pre-number: {$preNumber}{$midNumber}";
+            return $this->error($message, self::RES_NOT_DEFINED_PRE_NUMBERS);
         } catch (\Exception $e) {
-            return $this->error(
-                ["pre_number" => $e->getMessage()],
-                422
-            );
+            return $this->error($e->getMessage(), self::RES_ERROR);
         }
-        return response()->json(
-            $result
-        );
+
+        return ($result)
+            ? response()->json($result, self::RES_OK)
+            : $this->error(["No Result"], self::RES_NOT_FIND);
 
     }
 
     /**
-     * @param $errors
-     * @param $code
+     * @param $error
+     * @param null $resultCode
      * @return \Illuminate\Http\JsonResponse
      */
-    private function error($errors, $code)
+    private function error($error, $resultCode = null)
     {
         return response()->json([
             "success" => false,
-            "code" => $code,
-            "errors" => $errors
-        ], $code);
+            "code" => $resultCode,
+            "errors" => $error
+        ], 400);
+    }
+
+    /**
+     * @return string[]
+     */
+    private function searchRequestValidationRules(): array
+    {
+        return [
+            "pre_number" => "required|digits:3",
+            "mid_number" => "required|digits:4",
+            "number" => "required",
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,10 @@ Route::get('/import', function () {
     \Maatwebsite\Excel\Facades\Excel::import(new \App\Imports\ImportNumbers,\Illuminate\Support\Facades\Storage::path("ngn.xlsx"));
 });
 
-
 Route::get("/test",function(){
     $ngn = new \Hsy\Ngn\Center();
     $ngn->search("021","9130","2222");
 });
-
 
 // api response
 /*
